df4755feb898bf16f628719d9d019e4b1c696ee0
Vikas Pandey <86400964+vikaspandey18@users.noreply.github.com>
2021-08-08 13:55:23 +0530

    Changing the localserver to online server

--- includes/conn.php
+++ includes/conn.php
@@ -1,4 +1,11 @@
 <?php 
-$conn = mysqli_connect('localhost','root','','stddatabase') or die('Unable to connect to database');
 
+//Database to Work with on local Host
+
+// $conn = mysqli_connect('localhost','root','','stddatabase') or die('Unable to connect to database');
+
+
+//Database to Work with online Server
+
+$conn = mysqli_connect('remotemysql.com', 'WTduVtypQq', 'qZwpt1Z3JF','WTduVtypQq') or die("Unable To Connect To Database");
 ?>
